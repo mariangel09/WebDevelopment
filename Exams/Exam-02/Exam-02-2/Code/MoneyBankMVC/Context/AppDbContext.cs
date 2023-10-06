9635c2dca4ca0c0e120e9a918eacdfa743b05992
mariangel09 <angelplazamima@gmail.com>
2023-10-05 21:22:56 -0500

    CRUDCREATE

---
+++ Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoneyBankMVC.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
+
